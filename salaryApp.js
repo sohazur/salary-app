44d7001338b446ff81bf7ec27acd271f2c8c61fb
Sohazur <mdsohazurislam@gmail.com>
2022-01-16 03:29:56 +0400

    Name and price will be cleared from search field

--- salaryApp.js
+++ salaryApp.js
@@ -89,6 +89,8 @@ function addRecordHandler() {
   }
 
   addRecord(name, salary);
+  document.getElementById("name").value = "";
+  document.getElementById("salary").value = "";
 }
 
 function addRecord(name, salary) {
